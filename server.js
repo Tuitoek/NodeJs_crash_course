f66dd1b52357487b849c8a71583920867e1c4142
Tuitoek <sjtoek@gmail.com>
2026-07-22 13:24:55 +0300

    Add public html file

--- server.js
+++ server.js
@@ -1,17 +1,27 @@
 import http from "http";
+import fs from 'fs/promises'
 const PORT = process.env.PORT;
 
 const server = http.createServer((req, res) => {
- 
-  if (req.url === "/") {
-    res.writeHead(200, { "content-type": "text.html" });
-    res.end("<h1> Home Page</h1>");
-  } else if (req.url === '/about'){
-     res.writeHead(200, { "content-type": "text.html" });
-    res.end("<h1> About</h1>");
-  }else{
-    res.writeHead(404, { "content-type": "text.html" });
-    res.end("<h1> Not Found</h1>");
+  try {
+    //Check if GET Request
+    if (req.method === "GET") {
+      if (req.url === "/") {
+        res.writeHead(200, { "content-type": "text.html" });
+        res.end("<h1> Home Page</h1>");
+      } else if (req.url === "/about") {
+        res.writeHead(200, { "content-type": "text.html" });
+        res.end("<h1> About</h1>");
+      } else {
+        res.writeHead(404, { "content-type": "text.html" });
+        res.end("<h1> Not Found</h1>");
+      }
+    } else {
+      throw new Error("Method not allowed");
+    }
+  } catch (error) {
+    res.writeHead(500, { "content-type": "text/plain" });
+    res.end("Server Error");
   }
 });
 
