0e1c4760bfc03118c2e2da8cc2e637b463e8bf4d
Tuitoek <sjtoek@gmail.com>
2026-08-04 15:22:57 +0300

    Os methods

---
+++ osDemo.js
@@ -0,0 +1,13 @@
+import os from 'os';
+
+// Get user info 
+console.log('User Info:', os.userInfo());
+
+// Total Memory
+console.log('Total Memory:', os.totalmem());
+
+// Free Memory
+console.log('Free Memory:', os.freemem());
+
+// cpus()
+console.log('CPU Info:', os.cpus());
